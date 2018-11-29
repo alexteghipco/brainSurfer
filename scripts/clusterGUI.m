738c16e3b1df2af238dc4de85ef803aee8fcef7b
Auditory and Language Neuroscience Lab <30272795+ALNLab@users.noreply.github.com>
2018-11-28 17:02:43 -0800

    Update clusterGUI.m

--- scripts/clusterGUI.m
+++ scripts/clusterGUI.m
@@ -1,27 +1,6 @@
 function varargout = clusterGUI(varargin)
-% CLUSTERGUI MATLAB code for clusterGUI.fig
-%      CLUSTERGUI, by itself, creates a new CLUSTERGUI or raises the existing
-%      singleton*.
-%
-%      H = CLUSTERGUI returns the handle to a new CLUSTERGUI or the handle to
-%      the existing singleton*.
-%
-%      CLUSTERGUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in CLUSTERGUI.M with the given input arguments.
-%
-%      CLUSTERGUI('Property','Value',...) creates a new CLUSTERGUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before clusterGUI_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to clusterGUI_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help clusterGUI
-
+% clusterGUI
+% Alex Teghipco // alex.teghipco@uci.edu // 11/28/18
 % Last Modified by GUIDE v2.5 08-Nov-2018 00:35:56
 
 % Begin initialization code - DO NOT EDIT
