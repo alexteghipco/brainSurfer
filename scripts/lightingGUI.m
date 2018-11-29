d1d3192f2ae02643cceb57b0d6616fd8248c63fc
Auditory and Language Neuroscience Lab <30272795+ALNLab@users.noreply.github.com>
2018-11-28 17:08:35 -0800

    Update lightingGUI.m

--- scripts/lightingGUI.m
+++ scripts/lightingGUI.m
@@ -1,26 +1,6 @@
 function varargout = lightingGUI(varargin)
-% LIGHTINGGUI MATLAB code for lightingGUI.fig
-%      LIGHTINGGUI, by itself, creates a new LIGHTINGGUI or raises the existing
-%      singleton*.
-%
-%      H = LIGHTINGGUI returns the handle to a new LIGHTINGGUI or the handle to
-%      the existing singleton*.
-%
-%      LIGHTINGGUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in LIGHTINGGUI.M with the given input arguments.
-%
-%      LIGHTINGGUI('Property','Value',...) creates a new LIGHTINGGUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before lightingGUI_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to lightingGUI_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help lightingGUI
+% GUI to control lighting of underlays and overlays
+% Alex Teghipco // alex.teghipco@uci.edu // 11/28/18
 
 % Last Modified by GUIDE v2.5 09-Nov-2018 13:29:32
 
